f09bce9242b07d8584cc2d072fe0fd5e81c1654d
Yeimer Alberto <mr.yeimeralberto@gmail.com>
2023-02-24 08:38:52 -0300

    Layout de experiencia laboral

--- portfolioAP/src/app/app.module.ts
+++ portfolioAP/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
