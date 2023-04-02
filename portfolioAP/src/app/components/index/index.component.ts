4510b49f97c942ba4574fe7faea050c9298a6910
Yeimer Alberto <mr.yeimeralberto@gmail.com>
2023-04-02 19:02:37 -0300

    Diseñando el index.component para el ruteo

--- portfolioAP/src/app/components/index/index.component.ts
+++ portfolioAP/src/app/components/index/index.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginService } from 'src/app/services/auth/login.service';
+import { User } from 'src/app/services/auth/user';
 
 @Component({
   selector: 'app-index',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent {
+  userLoginOn: boolean = false;
+  userData?: User;
+
+  //Subscripción del Servicio.
+  constructor(private LoginService: LoginService) { }
+
+  //Buena práctica, desubscribirse, para evitar fugas de memoria.
+  ngOnDestroy():void {
+    this.LoginService.currentUserData.unsubscribe();
+    this.LoginService.currentUserLoginOn.unsubscribe();
+  }
+
+  //Al iniciar, la subscripción y asignación, de acuerdo al *ngIf
+  ngOnInit():void {
+    this.LoginService.currentUserLoginOn.subscribe({
+      next: (userLoginOn) => {
+        this.userLoginOn=userLoginOn;
+      }
+    })
+
+    //También subcribimos la data pero debe crearse la varaible e importar User.
+    this.LoginService.currentUserData.subscribe({
+      next: (userData) => {
+        this.userData=userData;
+      }
+    })
+  }
 
 }
