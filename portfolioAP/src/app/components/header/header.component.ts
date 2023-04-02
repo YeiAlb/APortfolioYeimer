72c3ec09ccc674b603395258d28f309081e6e9d7
Yeimer Alberto <mr.yeimeralberto@gmail.com>
2023-04-02 19:00:34 -0300

    Arreglando la navbar en el header component

--- portfolioAP/src/app/components/header/header.component.ts
+++ portfolioAP/src/app/components/header/header.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { LoginService } from 'src/app/services/auth/login.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent {
+  userLoginOn: boolean = false; //Para loguearse. En el component.html se trabaja con las directivas estructurales *ngIf y se especifican las variables --> !userLoginOn && userLoginOn
+
+  //Subscripción del Servicio de Login.
+  constructor(private LoginService: LoginService) { }
+
+//Buena práctica, desubscribirse, para evitar fugas de memoria.
+ngOnDestroy():void {
+  this.LoginService.currentUserLoginOn.unsubscribe();
+}
+
+  ngOnInit():void { //Al iniciar, la subscripción y asignación, de acuerdo al *ngIf
+    this.LoginService.currentUserLoginOn.subscribe({
+      next: (userLoginOn) => {
+        this.userLoginOn=userLoginOn;
+      }
+    })
+  }
 
 }
