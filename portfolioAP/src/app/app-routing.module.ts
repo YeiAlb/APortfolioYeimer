b4123a1724a77e8d0e6eb0538f155d93ecae74b3
Yeimer Alberto <mr.yeimeralberto@gmail.com>
2023-04-02 18:47:13 -0300

    Imporntando y concretando los componentes de las rutas en app-routing

--- portfolioAP/src/app/app-routing.module.ts
+++ portfolioAP/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
 import { Error404Component } from './components/error404/error404.component';
+import { LoginComponent } from './components/login/login.component';
 
+// Rutas parametrizadas, rutas hijas, por defecto, etc.
 const routes: Routes = [
-  {path:'', component: IndexComponent},
-  // // {path: '**', component: Error404Component}
+  { path:'inicio', component: IndexComponent },
+  { path:'iniciar-sesion', component: LoginComponent },
+  { path: '',   redirectTo: 'inicio', pathMatch: 'full' },
+  { path: '**', component: Error404Component }
 ];
 
 @NgModule({
