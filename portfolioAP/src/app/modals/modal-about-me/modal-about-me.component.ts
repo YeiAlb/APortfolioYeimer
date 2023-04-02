4998ab47dfde6a995738312db5eef804c622794f
Yeimer Alberto <mr.yeimeralberto@gmail.com>
2023-04-02 19:16:13 -0300

    Creacioón del modal-about-me

---
+++ portfolioAP/src/app/modals/modal-about-me/modal-about-me.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-about-me',
+  templateUrl: './modal-about-me.component.html',
+  styleUrls: ['./modal-about-me.component.css']
+})
+export class ModalAboutMeComponent {
+
+}
