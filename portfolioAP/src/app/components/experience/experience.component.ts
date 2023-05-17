e96780eeeb33d56189b34be71e376437ac3d4cd6
Yeimer Alberto <mr.yeimeralberto@gmail.com>
2023-05-17 06:57:55 -0300

    Funciones agregar y eliminar en Experiencia, trabajando modal experiencia

--- portfolioAP/src/app/components/experience/experience.component.ts
+++ portfolioAP/src/app/components/experience/experience.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { PortfolioService } from 'src/app/services/portfolio.service';
+import { ModalEducationComponent } from 'src/app/modals/modal-education/modal-education.component';
+import { Experience } from 'src/app/model/experience';
+import { ExperiencieService } from 'src/app/services/experiencie.service';
+// import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-experience',
@@ -7,19 +10,63 @@ import { PortfolioService } from 'src/app/services/portfolio.service';
   styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent implements OnInit {
+   // userLoginOn: boolean = false; //Para loguearse. En el component.html se trabaja con las directivas estructurales *ngIf y se especifican las variables --> !userLoginOn && userLoginOn
 
-    //Variable definida para el array experiencia laboral.
-    trabajosList: any;
+  
+  // Inicializamos VARIABLE TIPO ARRAY.
+  trabajosList: Experience[] = []; //Con el mismo nombre del MODEL ó ENTITY.
+  
 
-    //Se inyecta el servicio con la clase para exportar.
-    constructor(private dataBasePortfolio: PortfolioService) { }
+  // Se inyecta el SERVICIO de Experience (Experienceservice) con un ALIAS, con la clase para exportar.
+  constructor(private expeServi: ExperiencieService) { }
 
-    ngOnInit() {
-      //Subcripción del inyectable para el DataBinding o PropertyBinding.
-    this.dataBasePortfolio.obtenerDatos().subscribe (data => {
-      this.trabajosList = data.trabajos;
-    })
-    //Asignación de los datos requeridos del json a la variable.
-    //data es un alias.
+  //Se trae el método (cargarExperiencia) al inicio.
+  ngOnInit(): void {
+    this.cargarExperiencias();
   }
+
+  // -------MÉTODOS -------.
+  // Creamos métodos de tipo void(sin retorno) y llamamos al método en el Servicio.
+  // Subcripción del inyectable para realizar el DataBinding o PropertyBinding en el HTML.
+  // Usamos el ExperienceService con su alias y también como alias el data.
+  cargarExperiencias(): void {
+    this.expeServi.getExperienciasLaborales().subscribe (data => {
+      this.trabajosList = data
+    }); //data es un alias.
+  }
+
+
+
+  // Borrar experiencia laboral.
+  deleteExperienciaLaboral(id?: number) {
+    if (id !== undefined && confirm("Deseas eliminar esta Hard Skill?")) {
+      this.expeServi.deleteExperienciaLaboral(id).subscribe(data => {});
+      window.location.reload();
+      alert("Experiencia laboral eliminada correctamente");
+    }
+  }
+
+  
 }
+
+// Comenté todo lo que había hecho con el JSON, para dejar limpio el ts para argegar la base de datos que traemos con models y servicios del NetBeans. de aquí en adelante, también lo copié en pryectos AP.
+
+// export class ExperienceComponent implements OnInit {
+//   //Instanciamos la variable para usarla luego.
+//   //nombre:string = '';
+//   //Variable definida para el array experiencia laboral.
+//   //trabajosList: any = []; Si dejamos sólo any trae cualquier cosa.
+//   trabajosList: any;
+
+//   //Se inyecta el servicio con la clase para exportar.
+//   constructor(private dataBasePortfolio: PortfolioService) { }
+
+//   ngOnInit() {
+//     //Subcripción del inyectable para el DataBinding o PropertyBinding.
+//     this.dataBasePortfolio.obtenerDatos().subscribe (data => {
+//       this.trabajosList = data.experienciasLaborales;
+//       //this.nombre = data.nombre;
+//     })
+//   //Asignación de los datos requeridos del json a la variable.
+//   //data es un alias.
+//   }
--- portfolioAP/src/app/modals/modal-experience/modal-experience.component.ts
+++ portfolioAP/src/app/modals/modal-experience/modal-experience.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Experience } from 'src/app/model/experience';
+import { ExperiencieService } from 'src/app/services/experiencie.service';
 
 @Component({
   selector: 'app-modal-experience',
@@ -6,5 +9,147 @@ import { Component } from '@angular/core';
   styleUrls: ['./modal-experience.component.css']
 })
 export class ModalExperienceComponent {
+  trabajoForm: FormGroup;
+  trabajosList: Experience[] = [];
+  
+  // trabajo: any;
+  // id?: number;
 
+  //Objeto de FORMULARIO, con el servicio de fb crear los formControl y con la propiedad [formGroup] éste objeto de formularios y sus formcontrols, deben estar enlazados con el HTML.
+  //formControls y VALIDACIÓN, usamos VALIDACIÓN SINCRONICA.
+ 
+
+  //Importados e inyectados con alias se pueden consumir. FormBuilder: Formularios, Router: la función te envía al componente, experienceService: SERVICIO CREADO para comunicar con la API REST.
+  constructor(private fb: FormBuilder, private expeServi: ExperiencieService) {
+    //Grupo de controles para el formulario.
+    this.trabajoForm = this.fb.group({
+      id: [''],
+      logo_empresarial: ['', Validators.required],
+      url_empresa: [''],
+      nombre_empresa: ['', Validators.required],
+      fecha_inicio: ['', Validators.required],
+      fecha_fin: [''],
+      cargo: [''],
+    })
+  }
+  
+  ngOnInit(): void {
+    this.getTrabajos();
+  }
+
+  
+ // Propiedad Getter, para acceder a los atributos personalizados, son parte de trabajoForm.
+  //Campos y validaciones
+  get Logo_empresarial() {
+    return this.trabajoForm.get("logo_empresarial");
+  }
+ 
+  get Url_empresa() {
+    return this.trabajoForm.get("url_empresa");
+  }
+
+  get Nombre_empresa() {
+    return this.trabajoForm.get("nombre_empresa");
+  }
+
+  get Fecha_inicio() {
+    return this.trabajoForm.get("fecha_inicio");
+  }
+
+  get Fecha_fin() {
+    return this.trabajoForm.get("fecha_fin");
+  }
+
+  get Cargo() {
+    return this.trabajoForm.get("cargo");
+  }
+  
+   //Para cargar la lista de trabajos completa con el metodo List(), desde el EducationService, es llamda desde el onInit.
+   getTrabajos(): void {
+    this.expeServi.getExperienciasLaborales().subscribe({
+      next: (data) => {
+        this.trabajosList=data;
+        console.log("Trabajos cargados correctamente");
+      },
+      error: (e) => console.error(e),
+      complete: () => console.info('Completado')
+    })
+  }
+   
+     
+  findEstudio(id: number){
+    this.expeServi.findExperienciaLaboral(id).subscribe({
+      next: (data) => {
+        this.trabajoForm.setValue(data);
+      },
+      error: (e) => console.error(e),
+      complete: ()=> console.info('Completado')
+    });
+    console.log("Trabajo cargado correctamente");
+  }
+   
+   
+  onSave() {
+    let trabajo = this.trabajoForm.value;
+    if (this.trabajoForm.valid) {
+      this.expeServi.saveExperienciaLaboral(trabajo).subscribe({
+        next: (data) => {
+          this.reset();
+        },
+        error: (e) => console.error(e),
+        complete: () => console.info ('Completado')
+      })
+      window.location.reload();
+      alert("Trabajo cargado correctamente");
+    }
+    else {  
+      // Método para marcar todas las validaciones, con el siguiente método se marca si algo falla.
+      this.trabajoForm.markAllAsTouched();
+    }
+  }
+
+  onUpdate() {
+    let trabajo = this.trabajoForm.value;
+    if (this.trabajoForm.valid) {
+      this.expeServi.saveExperienciaLaboral(trabajo).subscribe({
+        next: (data) => {
+          this.reset();
+        },
+        error: (e) => console.error(e),
+        complete: () => console.info ('Completado')
+      })
+      window.location.reload();
+      alert("Trabajo cargado correctamente");
+    }
+    else {  
+      // Método para marcar todas las validaciones, con el siguiente método se marca si algo falla.
+      this.trabajoForm.markAllAsTouched();
+    }
+  }
+
+  reset() {
+    console.log("Se limpió el formulario");
+    this.trabajoForm.reset();
+  }
+
+
+  // Editar experiencia laboral.
+  // 1. Importamos ExperienceService.
+  // 2. Dentro del método update llamamos al update del servicio, que tiene su parámetro.
+  // 3. Usamos la función response si respuesta es exitosa o error, si no.
+
+  
+
+  // onDelete(id: number) {
+  //   if (confirm("Deseas eliminar este trabajo?")) {
+  //     this.expeServi.deleteExperienciaLaboral(id).subscribe(data => {});
+  //     window.location.reload();
+  //     alert("Trabajo eliminado correctamente");
+  //     this.getTrabajos();
+  //     this.trabajoForm.reset();
+  //     }
+  //   }
+  
+    
 }
+
