fae8272b5cbe2f812adfea92a561644801a82b02
Yeimer Alberto <mr.yeimeralberto@gmail.com>
2023-05-17 06:37:19 -0300

    Detalles de métodos y DataBinding en about me

--- portfolioAP/src/app/components/about-me/about-me.component.ts
+++ portfolioAP/src/app/components/about-me/about-me.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Persona } from 'src/app/model/persona';
+import { PersonaService } from 'src/app/services/persona.service';
 import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
@@ -7,18 +10,73 @@ import { PortfolioService } from 'src/app/services/portfolio.service';
   styleUrls: ['./about-me.component.css']
 })
 export class AboutMeComponent implements OnInit {
-  //Creamos una variable.
-  miPortfolio: any;
+  //userLoginOn: boolean = false; //Para loguearse. En el component.html se trabaja con las directivas estructurales *ngIf y se especifican las variables --> !userLoginOn && userLoginOn
 
-  constructor(private dataBasePortfolio: PortfolioService) { }
-  
-  ngOnInit(): void {
-    //Subcripción del inyectable, luego enlazar con DataBinding en el template.
-    this.dataBasePortfolio.obtenerDatos().subscribe(data => {
-      // console.log(data); // Observamos los datos llegando por consola.
-      //Asignación de la variable a los datos.
-      this.miPortfolio = data.personas;
-      //data es un alias.
-    } );
+  //Instanciamos una VARIABLE TIPO ARRAY para conectar luego en HTML con el DataBinding.
+  //estudiosList: any = []; Si dejamos sólo any trae cualquier cosa.
+  personas: Persona[]=[];
+  personaForm: FormGroup;
+
+    
+  //Se inyecta con un alias el SERVICIO de la CLASE para exportar.
+  constructor(private fb: FormBuilder, private persoServi: PersonaService) {
+    this.personaForm = this.fb.group({
+      id: [''],
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      ocupacion: ['', Validators.required],
+      sobre_mi: ['', Validators.required],
+      img_perfil: ['', Validators.required],
+    })
   }
+
+  ngOnInit():void {
+    this.cargarPersonas();
+  }
+
+  //Creamos el formulario para el modal 
+  
+  //En este método se hace la subcripción del inyectable para el DataBinding o PropertyBinding.
+  //El subscribe es un método que escucha, conecta el OBSERVER con algunos elementos que cambian.
+  cargarPersonas(): void {
+    this.persoServi.getPersonas().subscribe (
+      data => {
+        this.personas = data
+      });
+  }
+
+  findPersona(id: number){
+    this.persoServi.findPersona(id).subscribe({
+      next: (data) => {
+        this.personaForm.setValue(data);
+      },
+      error: (e) => console.error(e),
+      complete: ()=> console.info('complete')
+    });
+    console.log("Persona cargada correctamente");
+  }
+
+  
+
 }
+
+
+
+
+// Lo que hicimos con el JSON.
+// export class AboutMeComponent implements OnInit {
+//   //Creamos una variable.
+//   persona: any;
+
+//   constructor(private dataBasePortfolio: PortfolioService) { }
+  
+//   ngOnInit(): void {
+//     //Subcripción del inyectable, luego enlazar con DataBinding en el template.
+//     this.dataBasePortfolio.obtenerDatos().subscribe(data => {
+//       // console.log(data); // Observamos los datos llegando por consola.
+//       //Asignación de la variable a los datos.
+//       this.persona = data.personas;
+//       //data es un alias.
+//     } );
+//   }
+// }
--- portfolioAP/src/app/modals/modal-about-me/modal-about-me.component.ts
+++ portfolioAP/src/app/modals/modal-about-me/modal-about-me.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Persona } from 'src/app/model/persona';
+import { PersonaService } from 'src/app/services/persona.service';
 
 @Component({
   selector: 'app-modal-about-me',
@@ -6,5 +9,109 @@ import { Component } from '@angular/core';
   styleUrls: ['./modal-about-me.component.css']
 })
 export class ModalAboutMeComponent {
+  personaForm: FormGroup;
+  personas: Persona[] = [];
+  
+  // trabajo: any;
+  // id?: number;
+
+  //Objeto de FORMULARIO, con el servicio de fb crear los formControl y con la propiedad [formGroup] éste objeto de formularios y sus formcontrols, deben estar enlazados con el HTML.
+  //formControls y VALIDACIÓN, usamos VALIDACIÓN SINCRONICA.
+ 
+
+  //Importados e inyectados con alias se pueden consumir. FormBuilder: Formularios, Router: la función te envía al componente, personaService: SERVICIO CREADO para comunicar con la API REST.
+  constructor(private fb: FormBuilder, private persoServi: PersonaService) {
+    //Grupo de controles para el formulario.
+    this.personaForm = this.fb.group({
+      id: [''],
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      ocupacion: ['', Validators.required],
+      sobre_mi: ['', Validators.required],
+      img_perfil: ['', Validators.required],
+    })
+  }
+  
+  ngOnInit(): void {
+    this.getPersonas();
+  }
+
+  
+ // Propiedad Getter, para acceder a los atributos personalizados, son parte de trabajoForm.
+  //Campos y validaciones
+  get Nombre() {
+    return this.personaForm.get("nombre");
+  }
+ 
+  get Apellido() {
+    return this.personaForm.get("apellido");
+  }
+
+  get Ocupacion() {
+    return this.personaForm.get("ocupacion");
+  }
+
+  get Sobre_mi() {
+    return this.personaForm.get("sobre_mi");
+  }
+
+  get Img_perfil() {
+    return this.personaForm.get("img_perfil");
+  }
+
+
+  
+   //Para cargar la lista de trabajos completa con el metodo List(), desde el EducationService, es llamda desde el onInit.
+   getPersonas(): void {
+    this.persoServi.getPersonas().subscribe({
+      next: (data) => {
+        this.personas=data;
+        console.log("Personas cargadas correctamente");
+      },
+      error: (e) => console.error(e),
+      complete: () => console.info('Completado')
+    })
+  }
+
+
+  reset() {
+    console.log("Se limpió el formulario");
+    this.personaForm.reset();
+  }
 
 }
+
+  //Debería trababajar con Persona DTO, porque me da el error de atributos que faltan en persona, correo y calve, y no debería traerlos. Probaré con patchValue a ver.
+  // //Guardar edición.
+  // onUpdate() {
+  //   //Obtenenmos los datos o valores ingresados por el usario en el modal del formulario 
+  //   const valorNuevo = this.personaForm.value();
+
+  //   //Actualizar los datos de la persona.
+  //   const personaActualizada = {
+  //     id: valorNuevo.id,
+  //     nombre: valorNuevo.nombre,
+  //     apellido: valorNuevo.apellido,
+  //     ocupacion: valorNuevo.ocupacion,
+  //     sobre_mi: valorNuevo.sobre_mi,
+  //     img_perfil: valorNuevo.img_perfil,
+  //   };
+
+  //   //Guardamos los cambios en el servicio.
+  //   this.persoServi.updatePersona(personaActualizada)
+  // }
+
+
+
+  // onDelete(id: number) {
+  //   if (confirm("Deseas eliminar este trabajo?")) {
+  //     this.persoServi.deletePersona(id).subscribe(data => {});
+  //     window.location.reload();
+  //     alert("Persona eliminada correctamente");
+  //     this.getTrabajos();
+  //     this.personaForm.reset();
+  //     }
+  //   }
+  
+    
+
