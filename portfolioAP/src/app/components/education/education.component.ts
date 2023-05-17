ee049c5a1c1019c16a09eaeba88701843c41d26e
Yeimer Alberto <mr.yeimeralberto@gmail.com>
2023-05-17 06:42:15 -0300

    Funciones para educación: agregar y borrar

--- portfolioAP/src/app/components/education/education.component.ts
+++ portfolioAP/src/app/components/education/education.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { PortfolioService } from 'src/app/services/portfolio.service';
+import { Education } from 'src/app/model/education';
+import { EducationService } from 'src/app/services/education.service';
+// import { LoginService } from 'src/app/services/auth/login.service';
+// import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-education',
@@ -7,19 +10,58 @@ import { PortfolioService } from 'src/app/services/portfolio.service';
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent implements OnInit {
+  //userLoginOn: boolean = false; //Para loguearse. En el component.html se trabaja con las directivas estructurales *ngIf y se especifican las variables --> !userLoginOn && userLoginOn
 
-  //Variable definida para el array.
-  estudiosList: any;
-
-  //Se inyecta el servicio con la clase para exportar.
-  constructor(private dataBasePortfolio: PortfolioService) { }
+  //Instanciamos una VARIABLE TIPO ARRAY para conectar luego en HTML con el DataBinding.
+  //estudiosList: any = []; Si dejamos sólo any trae cualquier cosa.
+  estudiosList: Education[] = [];
+    
+  //Se inyecta con un alias el SERVICIO de la CLASE para exportar.
+  constructor(private educaServi: EducationService) { }
 
   ngOnInit():void {
-    //Subcripción del inyectable para el DataBinding o PropertyBinding.
-    this.dataBasePortfolio.obtenerDatos().subscribe (data => {
-      this.estudiosList = data.estudios;
-    })
-    //Asignación de los datos requeridos del json a la variable.
-    //data es un alias.
+    this.cargarEstudios();
   }
+  
+  //En este método se hace la subcripción del inyectable para el DataBinding o PropertyBinding.
+  //El subscribe es un método que escucha, conecta el OBSERVER con algunos elementos que cambian.
+  cargarEstudios(): void {
+    this.educaServi.getEstudios().subscribe (data => {
+      this.estudiosList = data;
+    })
+  }
+
+  findEstudio(id?: number){
+    if (id !== undefined){
+      this.educaServi.findEstudio(id).subscribe(data => {});
+    }
+  }
+    
+
+  deleteEstudio(id?: number) {
+    if (id !== undefined && confirm("Deseas eliminar este estudio?")) {
+        this.educaServi.deleteEstudio(id).subscribe(data => {});
+        window.location.reload();
+        alert("Estudio eliminado correctamente");
+      }
+    }
 }
+
+  // //Instanciamos la variable para usarla luego.
+  // //nombre:string = '';
+  // //Variable definida para el array estudio.
+  // //estudiosList: any = []; Si dejamos sólo any trae cualquier cosa.
+  // //Variable definida para el array.
+  // estudiosList: any;
+
+  // //Se inyecta el servicio con la clase para exportar.
+  // constructor(private dataBasePortfolio: PortfolioService) { }
+
+  // ngOnInit():void {
+  //   //Subcripción del inyectable para el DataBinding o PropertyBinding.
+  //   this.dataBasePortfolio.obtenerDatos().subscribe (data => {
+  //     this.estudiosList = data.estudios;
+  //     //this.nombre = data.nombre;
+  //   })
+  //   //Asignación de los datos requeridos del json a la variable.
+  //   //data es un alias.
--- portfolioAP/src/app/modals/modal-education/modal-education.component.ts
+++ portfolioAP/src/app/modals/modal-education/modal-education.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Education } from 'src/app/model/education';
+import { EducationService } from 'src/app/services/education.service';
 
 @Component({
   selector: 'app-modal-education',
@@ -6,5 +10,106 @@ import { Component } from '@angular/core';
   styleUrls: ['./modal-education.component.css']
 })
 export class ModalEducationComponent {
+  estudioForm: FormGroup;
+  estudiosList: Education[] = [];
+  
+  // estudio!: Education;
+  // id?: number;
+
+  //Objeto de FORMULARIO, con el servicio de fb crear los formControl y con la propiedad [formGroup] éste objeto de formularios y sus formcontrols, deben estar enlazados con el HTML.
+  //formControls y VALIDACIÓN, usamos VALIDACIÓN SINCRONICA.
+ 
+
+  //Importados e inyectados con alias se pueden consumir. FormBuilder: Formularios, Router: la función te envía al componente, educationService: SERVICIO CREADO para comunicar con la API REST.
+  constructor(private fb: FormBuilder, private educaServi: EducationService, private router: Router) {
+    //Grupo de controles para el formulario.
+    this.estudioForm = this.fb.group({
+      id: [''],
+      logo_institucional: ['', Validators.required],
+      url_institucion: [''],
+      nombre_institucion: ['', Validators.required],
+      titulo_academico: ['', Validators.required],
+      fecha_inicio: [''],
+      fecha_fin: [''],
+    })
+  }
+  
+  ngOnInit(): void {
+    this.getEstudios();
+  }
+
+  
+ // Propiedad Getter, para acceder a los atributos personalizados, son parte de estudioForm.
+  //Campos y validaciones
+  get Logo_institucional() {
+    return this.estudioForm.get("logo_institucional");
+  }
+ 
+  get Url_institucion() {
+    return this.estudioForm.get("url_institucion");
+  }
+  
+  get Nombre_institucion() {
+    return this.estudioForm.get("nombre_institucion");
+  }
+
+  get Titulo_academico() {
+    return this.estudioForm.get("titulo_academico");
+  }
+
+  get Fecha_inicio() {
+    return this.estudioForm.get("fecha_inicio");
+  }
+
+  get Fecha_fin() {
+    return this.estudioForm.get("fecha_fin");
+  }
+  
+   //Para cargar la lista de estudios completa con el metodo: (List<Estudios> getEstudios()), desde el EducationService, es llamada en el onInit.
+  getEstudios(): void {
+    this.educaServi.getEstudios().subscribe({
+      next: (data) => {
+        this.estudiosList=data;
+        console.log("Estudios cargados correctamente");
+      },
+      error: (e) => console.error(e),
+      complete: () => console.info('Completado')
+    })
+  }
+    
+   
+  saveEstudio() {
+    let estudio = this.estudioForm.value;
+    if (this.estudioForm.valid) {
+      this.educaServi.saveEstudio(estudio).subscribe({
+        next: (data) => {
+          this.reset();
+        },
+        error: (e) => console.error(e),
+        complete: () => console.info ('Completado')
+      })
+      window.location.reload();
+      alert("Estudio cargado correctamente");
+    }
+    else {  
+      // Método para marcar todas las validaciones, con el siguiente método se marca si algo falla.
+      this.estudioForm.markAllAsTouched();
+    }
+  }
+
+
+  updateEstudio():void{
+    this.educaServi.updateEstudio(this.estudioForm.value).subscribe(data => {
+      alert("Estudio modificado.");
+      this.router.navigate(['']);
+    }
+    )
+  }
+    
+
+  reset() {
+    console.log("Se limpió el formulario");
+    this.estudioForm.reset();
+  }
 
 }
